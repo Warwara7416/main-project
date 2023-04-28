d4f67af95d9b232df736c058a1086cffcb1f82ea
Warwara7416 <pavel.002@mail.ru>
2023-04-28 16:18:43 +0500

    async await

--- fetch.js
+++ fetch.js
@@ -3,28 +3,28 @@ const output = document.querySelector(".profile");
 
 formAuth.addEventListener("submit", auth);
 
-function auth(event) {
+async function auth(event) {
   event.preventDefault(); //Отменяем отправку формы
+
   let data = new FormData(formAuth);
-  fetch("api/auth.php", {
+
+  const response = await fetch("api/auth.php", {
     method: 'POST',
     'Content-Type': 'application/json',
     body: data
-  }).then(
-    (response) => {
-      return response.json();
-  }).then(
-      (json) => {
-        console.log(json);
-        if (json.status) {
-          output.innerHTML = "Вы авторизованы как " + json.name;
-          formAuth.style.display = "none";
-        }
-        else {
-          let p = document.createElement("p");
-          p.innerHTML = "Ошибка авторизации";
-          output.prepend(p);
-        }
-      })
+  });
 
+  json = await response.json();
+
+  console.log(json);
+
+  if (json.status) {
+    output.innerHTML = "Вы авторизованы как " + json.name;
+    formAuth.style.display = "none";
+  }
+  else {
+    let p = document.createElement("p");
+    p.innerHTML = "Ошибка авторизации";
+    output.prepend(p);
+  }
 }
